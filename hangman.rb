e0c993eb5e52ed2e8b800e3ae0af3c3f59a2fe46
Harry Brodsky <hbrodsk1@binghamton.edu>
2016-06-07 21:34:59 -0400

    Incorrect guess array shows on every turn

--- hangman.rb
+++ hangman.rb
@@ -19,6 +19,7 @@ class Hangman
 		@blank_spaces = ["_ "] * @word_to_be_guessed.size
 		@current_guess = ""
 		@incorrectly_guessed_letters = []
+		@correctly_guessed_letters = []
 		@turns_left = 6
 		
 		create_blank_spaces
@@ -38,7 +39,11 @@ class Hangman
 	def user_letter
 		@current_guess = gets.chomp
 
-		validate_input(@current_guess.downcase)
+		if @correctly_guessed_letters.include?(@current_guess.downcase) || @incorrectly_guessed_letters.include?(@current_guess.downcase)
+			already_guessed_letter(@current_guess)
+		else
+			validate_input(@current_guess.downcase)
+		end
 	end
 
 	def validate_input(letter)
@@ -51,14 +56,29 @@ class Hangman
 		end
 	end
 
+	def already_guessed_letter(letter)
+		puts "\nYou have already guessed #{letter}. Please choose another letter"
+		puts @gallow.gallow_to_draw_on_turn(@turn_number)
+		print "\n#{@blank_spaces.join(" ")}"
+		puts "\n\nIncorrect Letters:"
+		puts @incorrectly_guessed_letters.join(", ")
+
+		user_letter
+	end
+
 	def reenter_input(letter)
 		puts "\nSorry, #{letter} is not a valid input. Please choose a letter from A-Z"
+		puts @gallow.gallow_to_draw_on_turn(@turn_number)
+		print "\n#{@blank_spaces.join(" ")}"
+		puts "\n\nIncorrect Letters:"
+		puts @incorrectly_guessed_letters.join(", ")
 
 		user_letter
 	end
 
 	def check_for_user_letter_in_word(letter)
 		if @word_to_be_guessed.include?(@current_guess.downcase)
+			@correctly_guessed_letters << @current_guess.downcase
 			replace_blank_with_correct_letter(@current_guess.downcase)
 		else
 			incorrect_guess
@@ -71,6 +91,7 @@ class Hangman
 				@blank_spaces[index] = @current_guess
 			end
 		end
+		puts @gallow.gallow_to_draw_on_turn(@turn_number)
 		print "\n#{@blank_spaces.join(" ")}"
 		guess_again
 	end
@@ -80,8 +101,15 @@ class Hangman
 		@turns_left -= 1
 		@turn_number += 1
 
-		puts @gallow.gallow_to_draw_on_turn(@turn_number)
-		guess_again
+		if @turns_left == 0
+			puts @gallow.gallow_to_draw_on_turn(@turn_number)
+			puts "\n\nSorry you are all out of guesses. Would you like to play again? (Y / N)"
+			play_again
+		else
+			puts @gallow.gallow_to_draw_on_turn(@turn_number)
+			print "\n#{@blank_spaces.join(" ")}"
+			guess_again
+		end
 	end
 
 	def guess_again
@@ -90,6 +118,19 @@ class Hangman
 
 		puts "\n\nPlease guess another letter"
 		user_letter
+	end
+
+	def play_again
+		new_game = gets.chomp
+
+		if new_game.downcase == "y"
+			Hangman.new(@name)
+		elsif new_game.downcase == "n"
+			exit
+		else
+			puts "Please enter Y or N"
+			play_again
+		end
 	end
 end
 
